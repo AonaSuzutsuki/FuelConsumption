5ee4a739686209a37e91903f8df43567537bf65c
涼月蒼菜 <rvvfake.org@gmail.com>
2019-07-04 17:35:04 +0900

    added test proj.

---
+++ FuelConsumption2/Models/AddFuelConsumptionPageModel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using FuelConsumption2.Views;
+using Prism.Mvvm;
+
+namespace FuelConsumption2.Models
+{
+    public class AddFuelConsumptionPageModel : BindableBase
+    {
+        public ObservableCollection<string> FuelTypes { get; set; }
+
+        private double pricePerLitter;
+        public double PricePerLitter
+        {
+            get => pricePerLitter;
+            set => SetProperty(ref pricePerLitter, value);
+        }
+
+        private double litter;
+        public double Litter
+        {
+            get => litter;
+            set => SetProperty(ref litter, value);
+        }
+
+        private double odo;
+        public double Odo
+        {
+            get => odo;
+            set => SetProperty(ref odo, value);
+        }
+
+        private DateTime date;
+        public DateTime Date
+        {
+            get => date;
+            set => SetProperty(ref date, value);
+        }
+
+        private string memo;
+        public string Memo
+        {
+            get => memo;
+            set => SetProperty(ref memo, value);
+        }
+
+        private int fuelTypeSelectedIndex;
+        public int FuelTypeSelectedIndex
+        {
+            get => fuelTypeSelectedIndex;
+            set => SetProperty(ref fuelTypeSelectedIndex, value);
+        }
+
+        private string actionButtonText = "Add";
+        public string ActionButtonText
+        {
+            get => actionButtonText;
+            set => SetProperty(ref actionButtonText, value);
+        }
+
+        private bool editMode;
+        public bool EditMode
+        {
+            get => editMode;
+            set
+            {
+                if (value)
+                    ActionButtonText = "Edit";
+                else
+                    ActionButtonText = "Add";
+                editMode = value;
+            }
+        }
+
+        //public FuelConsumptionInfo FuelConsumptionInfo { get; set; }
+        public Action<FuelConsumptionInfo> ChangedItemAction { get; set; }
+        public Action<FuelConsumptionInfo> ItemAddAction { get; set; }
+        public Action SaveAction { get; set; }
+
+        public AddFuelConsumptionPageModel()
+        {
+            FuelTypes = new ObservableCollection<string>
+            {
+                "レギュラー",
+                "ハイオク",
+                "軽油"
+            };
+            FuelTypeSelectedIndex = 0;
+        }
+
+        public void AddToMenuItems()
+        {
+            if (EditMode)
+            {
+                //if (FuelConsumptionInfo != null)
+                //{
+                //    FuelConsumptionInfo.PricePerLitter = PricePerLitter;
+                //    FuelConsumptionInfo.Litter = Litter;
+                //    FuelConsumptionInfo.Odo = Odo;
+                //    FuelConsumptionInfo.Date = Date;
+                //    FuelConsumptionInfo.Memo = Memo;
+                //}
+                ChangedItemAction(new FuelConsumptionInfo
+                {
+                    PricePerLitter = PricePerLitter,
+                    Litter = Litter,
+                    Odo = Odo,
+                    Date = Date,
+                    Memo = Memo
+                });
+            }
+            else
+            {
+                ItemAddAction(new FuelConsumptionInfo
+                {
+                    PricePerLitter = PricePerLitter,
+                    Litter = Litter,
+                    Odo = Odo,
+                    Date = Date,
+                    Memo = Memo
+                });
+            }
+
+            SaveAction();
+            NavigationClass.CloseModal();
+        }
+    }
+}
---
+++ FuelConsumption2UnitTest/Models/FuelConsumptionInfoTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FuelConsumption2.Models;
+using NUnit.Framework;
+
+namespace FuelConsumption2UnitTest.Models
+{
+    [TestFixture]
+    public class FuelConsumptionInfoTest
+    {
+        [Test]
+        public void FuelConsumptionTest()
+        {
+            var value1 = new FuelConsumptionInfo
+            {
+                Odo = 6327,
+                Date = new DateTime(2019, 6, 9),
+                Litter = 5.17,
+                PricePerLitter = 153
+            };
+            var value2 = new FuelConsumptionInfo
+            {
+                Odo = 6512,
+                Date = new DateTime(2019, 7, 1),
+                Litter = 5.54,
+                PricePerLitter = 148
+            };
+
+            var exp = 33.393501805054152d.ToString("F2"); 
+            var act = value2.FuelConsumption(value1).ToString("F2");
+
+            Assert.AreEqual(exp, act);
+        }
+    }
+}
